3f817365cc7c99ac0254d4b6878a3ef409a2b62e
subham-20000901 <sinhasubh2000@gmail.com>
2025-12-23 20:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+	s1 = s1.toLowerCase();
+	s2 = s2.toLowerCase();
+ for(let i=0; i<=s1.length-s2.length; i++){
+	 
+	 let match = true;
+	 for(let j=0; j<s2.length; j++){
+		 if(s1[i+j] !== s2[j]){
+			 match = false;
+			 break;
+		 }
+	 }
+	 
+	 if(match) return i;
+	 
+ }
+	return -1;
+	
+
+ }
+	 
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+ alert(indexOfIgnoreCase(s1, s2));
+
